63fc5d10dd9600fd101c73b4e3ce4d018212b177
kakiii <kakiatms@outlook.com>
2023-05-09 18:32:23 +0200

    feat: upate checkout logic

--- order/app.py
+++ order/app.py
@@ -1,6 +1,8 @@
 import os
 import atexit
 import json
+import requests
+from collections import Counter
 
 from flask import Flask, abort
 import redis
@@ -61,6 +63,10 @@ def add_item(order_id, item_id):
 
     order_found = json.loads(db.get(order_id))
     order_found["items"].append(item_id)
+    # db.set(order_id, json.dumps(order_found))
+    # update total cost
+    item_price = requests.get(f"{gateway_url}/stock/find/{item_id}").json()["price"]
+    order_found["total_cost"] += item_price
     db.set(order_id, json.dumps(order_found))
 
     return {"CODE": 200}
@@ -79,6 +85,9 @@ def remove_item(order_id, item_id):
         abort(404, description=f"Item with id {item_id} not found in order with id {order_id}")
     else:
         order_found["items"].remove(item_id)
+        # update total cost
+        item_price = requests.get(f"{gateway_url}/stock/find/{item_id}").json()["price"]
+        order_found["total_cost"] -= item_price
         db.set(order_id, json.dumps(order_found))
         return {"CODE": 200}
 
@@ -97,4 +106,43 @@ def find_order(order_id):
 
 @app.post('/checkout/<order_id>')
 def checkout(order_id):
-    pass
+    # check if the order exists
+    if not db.exists(order_id):
+        abort(404, description=f"Order with id {order_id} not found")
+    
+    # retrieve the order from the database
+    order_found = json.loads(db.get(order_id))
+    
+    # transform list of items into a dictionary
+    items_count = Counter(order_found["items"])
+    checkout_possible = True
+    # query the stock service to check if the items are available
+    for item_id, count in items_count.items():
+        response = requests.get(f"{gateway_url}/stock/find/{item_id}")
+        if response.status_code != 200:
+            abort(response.status_code, description=response.json()["message"])
+        if response.json()["stock"] < count:
+            checkout_possible = False
+            break
+    
+    if not checkout_possible:
+        abort(400, description="Not enough stock for some items")
+    else:
+        # query payment service to check if the user has enough balance
+        total_cost = order_found["total_cost"]
+        response = requests.post(f"{gateway_url}/payment/pay/{order_found['user_id']}/{order_id}/{total_cost}")
+        if response.status_code != 200:
+            # revert the stock information
+            abort(response.status_code, description=response.json()["message"])
+        else:
+            
+            # update the stock information
+            for item_id, count in items_count.items():
+                requests.post(f"{gateway_url}/stock/remove/{item_id}/{count}")
+
+            # update the order information    
+            order_found["paid"] = True
+            order_found["total_cost"] = total_cost
+            return {"CODE": 200, "status": "Order paid successfully"}
+    
+

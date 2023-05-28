e0d16f04b8d978382b52ab2918ba363b5733d731
joeltohyiliang <joeltohyiliang99@gmail.com>
2023-05-28 17:45:15 +0200

    modified stock
    back to old code

--- stock/app.py
+++ stock/app.py
@@ -46,62 +46,23 @@ def find_item(item_id: str):
     
 
 
-# @app.post('/add/<item_id>/<amount>')
-# def add_stock(item_id: str, amount: int):
-#     # check if the item exists
-#     if not db.exists(item_id):
-#         abort(404, description=f"Item with id {item_id} not found")
-#     item_found = json.loads(db.get(item_id))
-#     item_found["stock"] = int(item_found["stock"])
-#     item_found["stock"] += int(amount)
-#     db.set(item_id, json.dumps(item_found))
-#     # return {"CODE": 200}
-#     # return {"status_code":200}
-#     response = app.response_class(
-#         response=item_found,
-#         status=200,
-#         mimetype='application/json'
-#     )
-#     return response
-
 @app.post('/add/<item_id>/<amount>')
 def add_stock(item_id: str, amount: int):
-    # Check if the item exists
+    # check if the item exists
     if not db.exists(item_id):
         abort(404, description=f"Item with id {item_id} not found")
-
-    # Retrieve the item from the database
     item_found = json.loads(db.get(item_id))
     item_found["stock"] = int(item_found["stock"])
-    
-    # Call coordination service to start a new transaction
-    response = requests.post(f"{gateway_url}/start_tx")
-    if response.status_code == 200:
-        conn_id = response.json()["conn_id"]
-        
-        # Update item stock in the transaction context
-        item_found["stock"] += int(amount)
-        db.set(item_id, json.dumps(item_found))
-        
-        # Prepare the transaction in the coordination service
-        response = requests.post(f"{gateway_url}/prepare/{conn_id}")
-        if response.status_code == 200:
-            # Store the updated item stock value
-            db.set(item_id, json.dumps(item_found))
-
-            # Commit the transaction
-            response = requests.post(f"{gateway_url}/commit_tx/{conn_id}")
-            if response.status_code == 200:
-                return {"message": "Stock added successfully"}
-            else:
-                # Handle commit failure
-                return {"error": "Failed to commit transaction"}, 500
-        else:
-            # Handle prepare failure
-            return {"error": "Failed to prepare transaction"}, 500
-    else:
-        # Handle transaction start failure
-        return {"error": "Failed to start transaction"}, 500
+    item_found["stock"] += int(amount)
+    db.set(item_id, json.dumps(item_found))
+    # return {"CODE": 200}
+    # return {"status_code":200}
+    response = app.response_class(
+        response=item_found,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 
 @app.post('/subtract/<item_id>/<amount>')
@@ -109,56 +70,16 @@ def remove_stock(item_id: str, amount: int):
     # check if the item exists
     if not db.exists(item_id):
         abort(404, description=f"Item with id {item_id} not found")
-    # Start the transaction
-    response = requests.post(f"{gateway_url}/start_tx")
-    if response.status_code == 200:
-        conn_id = response.json()["conn_id"]
-        # Get the current item stock value
-        item_found = json.loads(db.get(item_id))
-        item_found["stock"] = int(item_found["stock"])
-        if item_found["stock"] < int(amount):
-            abort(404, description=f"Not enough stock for item with id {item_id}")
-        else:
-            # Decrement the stock by the specified amount
-            item_found["stock"] -= int(amount)
-
-            # Prepare the transaction in the coordination service
-            response = requests.post(f"{gateway_url}/prepare/{conn_id}")
-            if response.status_code == 200:
-                # Store the updated item stock value
-                db.set(item_id, json.dumps(item_found))
-                # Commit the transaction
-                response = requests.post(f"{gateway_url}/commit_tx/{conn_id}")
-                if response.status_code == 200:
-                    return {"message": "Stock removed successfully"}
-                else:
-                    # Handle commit failure
-                    return {"error": "Failed to commit transaction"}, 500
-            else:
-                # Handle prepare failure
-                return {"error": "Failed to prepare transaction"}, 500
+    item_found = json.loads(db.get(item_id))
+    item_found["stock"] = int(item_found["stock"])
+    if item_found["stock"] < int(amount):
+        abort(404, description=f"Not enough stock for item with id {item_id}")
     else:
-        # Handle transaction start failure
-        return {"error": "Failed to start transaction"}, 500
-
-
-
-        
-# @app.post('/subtract/<item_id>/<amount>')
-# def remove_stock(item_id: str, amount: int):
-#     # check if the item exists
-#     if not db.exists(item_id):
-#         abort(404, description=f"Item with id {item_id} not found")
-#     item_found = json.loads(db.get(item_id))
-#     item_found["stock"] = int(item_found["stock"])
-#     if item_found["stock"] < int(amount):
-#         abort(404, description=f"Not enough stock for item with id {item_id}")
-#     else:
-#         item_found["stock"] -= int(amount)
-#         db.set(item_id, json.dumps(item_found))
-#         response = app.response_class(
-#         response="",
-#         status=200,
-#         mimetype='application/json'
-#     )
-#     return response
+        item_found["stock"] -= int(amount)
+        db.set(item_id, json.dumps(item_found))
+        response = app.response_class(
+        response="",
+        status=200,
+        mimetype='application/json'
+    )
+    return response

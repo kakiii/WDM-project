69b67469c6f7958ac0879a9d9877fa9a350c8d35
joeltohyiliang <joeltohyiliang99@gmail.com>
2023-05-28 16:48:00 +0200

    cooridnator
    test

--- coord/app.py
+++ coord/app.py
@@ -5,12 +5,12 @@ import requests
 from collections import Counter
 import uuid
 
-from flask import Flask, abort, jsonify
+from flask import Flask, jsonify
 import redis
 
 
 app = Flask("coord-service")
-
+gateway_url = os.environ['GATEWAY_URL']
 
 db: redis.Redis = redis.Redis(host=os.environ['REDIS_HOST'],
                               port=int(os.environ['REDIS_PORT']),
@@ -25,10 +25,77 @@ atexit.register(close_db_connection)
 def start_transaction():
     # conn_id = str(db.incr("transaction_id"))
     conn_id = str(uuid.uuid4())
-    db.set(conn_id, "STARTED")
+    db.set(conn_id, "PREPARE")
     return jsonify({'conn_id': conn_id}), 200
 
+@app.get('/status/<conn_id>')
+def get_transaction_status(conn_id):
+    if db.exists(conn_id):
+        status = db.get(conn_id).decode('utf-8')
+        status = db.get(conn_id)
+        return jsonify({'status': status})
+    else:
+        return jsonify({'error': 'Invalid connection ID'}), 400
 
+@app.post('/prepare/<conn_id>')
+def prepare_transaction(conn_id):
+    if db.exists(conn_id):
+        status = db.get(conn_id).decode('utf-8')
+        if status == "PREPARE":
+            db.set(conn_id, "READY")
+            return jsonify({'message': 'Prepare vote recorded'})
+        else:
+            return jsonify({'error': 'Invalid transaction status'}), 400
+    else:
+        return jsonify({'error': 'Invalid connection ID'}), 400
+
+@app.post('/acknowledge/<conn_id>')
+def acknowledge_transaction(conn_id):
+    if db.exists(conn_id):
+        status = db.get(conn_id).decode('utf-8')
+        if status == "COMMIT":
+            db.set(conn_id, "ACKNOWLEDGED")
+            return jsonify({'message': 'Acknowledgment recorded'})
+        else:
+            return jsonify({'error': 'Invalid transaction status'}), 400
+    else:
+        return jsonify({'error': 'Invalid connection ID'}), 400
+
+@app.post('/commit_tx/<conn_id>')
+def commit_transaction(conn_id):
+    if db.exists(conn_id):
+        status = db.get(conn_id).decode('utf-8')
+        if status == "READY" or status == "ACKNOWLEDGED":
+            db.set(conn_id, "COMMIT")
+            # Send commit message to participating services
+            response = requests.post(f"{gateway_url}/order/commit/{conn_id}")
+            if response.status_code == 200:
+                return jsonify({'message': 'Transaction committed successfully'})
+            else:
+                # Handle failure or error case
+                db.set(conn_id, "ABORT")
+                return jsonify({'error': 'Failed to commit transaction'}), 500
+        else:
+            return jsonify({'error': 'Invalid transaction status'}), 400
+    else:
+        return jsonify({'error': 'Invalid connection ID'}), 400
+
+@app.post('/cancel_tx/<conn_id>')
+def cancel_transaction(conn_id):
+    if db.exists(conn_id):
+        status = db.get(conn_id).decode('utf-8')
+        if status == "READY" or status == "ACKNOWLEDGED":
+            db.set(conn_id, "ABORT")
+            # Send abort message to participating services
+            response = requests.post(f"{gateway_url}/order/cancel/{conn_id}")
+            if response.status_code == 200:
+                return jsonify({'message': 'Transaction cancelled successfully'})
+            else:
+                # Handle failure or error case
+                return jsonify({'error': 'Failed to cancel transaction'}), 500
+        else:
+            return jsonify({'error': 'Invalid transaction status'}), 400
+                           
 @app.post('/exec/<conn_id>')
 # @app.route('/exec/<conn_id>', methods=['POST'])
 def execute_command(conn_id):
@@ -39,24 +106,24 @@ def execute_command(conn_id):
     else:
         return jsonify({'error': 'Invalid connection ID'}), 400
 
-# @app.route('/commit_tx/<conn_id>', methods=['POST'])
-@app.post('/commit_tx/<conn_id>')
-def commit_transaction(conn_id):
-    if db.exists(conn_id):
-        db.rpush(f"{conn_id}:commands", "COMMIT")
-        db.delete(conn_id)
-        return jsonify({'message': 'Transaction committed successfully'})
-    else:
-        return jsonify({'error': 'Invalid connection ID'}), 400
-@app.post('/cancel_tx/<conn_id>')
-# @app.route('/cancel_tx/<conn_id>', methods=['POST'])
-def cancel_transaction(conn_id):
-    if db.exists(conn_id):
-        db.rpush(f"{conn_id}:commands", "ROLLBACK")
-        db.delete(conn_id)
-        return jsonify({'message': 'Transaction cancelled successfully'})
-    else:
-        return jsonify({'error': 'Invalid connection ID'}), 400
+# # @app.route('/commit_tx/<conn_id>', methods=['POST'])
+# @app.post('/commit_tx/<conn_id>')
+# def commit_transaction(conn_id):
+#     if db.exists(conn_id):
+#         db.rpush(f"{conn_id}:commands", "COMMIT")
+#         db.delete(conn_id)
+#         return jsonify({'message': 'Transaction committed successfully'})
+#     else:
+#         return jsonify({'error': 'Invalid connection ID'}), 400
+# @app.post('/cancel_tx/<conn_id>')
+# # @app.route('/cancel_tx/<conn_id>', methods=['POST'])
+# def cancel_transaction(conn_id):
+#     if db.exists(conn_id):
+#         db.rpush(f"{conn_id}:commands", "ROLLBACK")
+#         db.delete(conn_id)
+#         return jsonify({'message': 'Transaction cancelled successfully'})
+#     else:
+#         return jsonify({'error': 'Invalid connection ID'}), 400
 
 # @app.route('/find/<item_id>', methods=['GET'])
 # def find_item(item_id):
@@ -69,3 +136,4 @@ def cancel_transaction(conn_id):
 #     item_found["stock"] = int(item_found["stock"])
 
 #     return jsonify({'CODE': 200, **item_found})
+
--- order/app.py
+++ order/app.py
@@ -71,9 +71,6 @@ def add_item(order_id, item_id):
     order_found = json.loads(db.get(order_id))
     order_found["items"].append(item_id)
 
-    # Decrease item dict stock level? TODO:
-
-
     # update total cost
     item_price = requests.get(f"{gateway_url}/stock/find/{item_id}").json()["price"]
     order_found["total_cost"] += item_price
@@ -110,8 +107,6 @@ def remove_item(order_id, item_id):
         )
         return response
 
-
-
 @app.get('/find/<order_id>')
 def find_order(order_id):
     # check if the order exists
@@ -129,49 +124,112 @@ def find_order(order_id):
     return jsonify(order_found), 200
 
 
+# @app.post('/checkout/<order_id>')
+# def checkout(order_id):
+#     # Check if the order exists
+#     if not db.exists(order_id):
+#         abort(404, description=f"Order with id {order_id} not found")
+    
+#     # Retrieve the order from the database
+#     order_found = json.loads(db.get(order_id))
+
+#     if order_found["paid"]:
+#         abort(400, description="Order has already been paid")
+#     # Transform list of items into a dictionary
+#     items_count = Counter(order_found["items"])
+#     # Query the stock service to check if the items are available
+#     for item_id, count in items_count.items():
+#         response = requests.get(f"{gateway_url}/stock/find/{item_id}")
+#         if response.json()["stock"] < count:
+#             abort(400, description=f"Not enough stock for item {item_id}")
+
+#     # Query payment service to check if the user has enough balance
+#     # total_cost = int(order_found["total_cost"])
+#     if 'total_cost' not in order_found:
+#         abort(400, description="Total cost not found")
+#     response= requests.post(f"{gateway_url}/payment/pay/{order_found['user_id']}/{order_id}/{int(order_found['total_cost'])}")
+#     if response.status_code >= 500:
+#         abort(response.status_code, description=f"Payment service error with url {response.url}, order: {order_found}")
+#     elif response.status_code >= 400:
+#         abort(response.status_code, description=f"Not enough balance, order: {order_found}")
+#     else:
+#         print("Payment successful")
+
+#     # Update the stock information and order information    
+#     for item_id, count in items_count.items():
+#         with requests.post(f"{gateway_url}/stock/subtract/{item_id}/{count}") as response:
+#             if response.status_code != 200:
+#                 abort(response.status_code, description="Stock update failed")
+
+#     order_found["paid"] = True
+#     # order_found["total_cost"] = total_cost
+#     db.set(order_id, json.dumps(order_found))
+
+#     return {"status": "success"}, 200
+
+
 @app.post('/checkout/<order_id>')
 def checkout(order_id):
     # Check if the order exists
     if not db.exists(order_id):
         abort(404, description=f"Order with id {order_id} not found")
-    
+
     # Retrieve the order from the database
     order_found = json.loads(db.get(order_id))
 
     if order_found["paid"]:
         abort(400, description="Order has already been paid")
-    
+
     # Transform list of items into a dictionary
     items_count = Counter(order_found["items"])
 
-    # Query the stock service to check if the items are available
-    for item_id, count in items_count.items():
-        response = requests.get(f"{gateway_url}/stock/find/{item_id}")
-        if response.json()["stock"] < count:
-            abort(400, description=f"Not enough stock for item {item_id}")
+    # Call the coordinator service to start a new transaction
+    response = requests.post(f"{gateway_url}/start_tx")
+    if response.status_code != 200:
+        abort(response.status_code, description="Failed to start the transaction")
 
-    # Query payment service to check if the user has enough balance
-    # total_cost = int(order_found["total_cost"])
-    if 'total_cost' not in order_found:
-        abort(400, description="Total cost not found")
-    response= requests.post(f"{gateway_url}/payment/pay/{order_found['user_id']}/{order_id}/{int(order_found['total_cost'])}")
-    if response.status_code >= 500:
-        abort(response.status_code, description=f"Payment service error with url {response.url}, order: {order_found}")
-    elif response.status_code >= 400:
-        abort(response.status_code, description=f"Not enough balance, order: {order_found}")
-    else:
-        print("Payment successful")
+    transaction_data = response.json()
+    conn_id = transaction_data['conn_id']
 
-    # Update the stock information and order information    
-    for item_id, count in items_count.items():
-        with requests.post(f"{gateway_url}/stock/subtract/{item_id}/{count}") as response:
+    try:
+        # Call the coordinator service to prepare the transaction
+        response = requests.post(f"{gateway_url}/prepare/{conn_id}")
+        if response.status_code != 200:
+            abort(response.status_code, description="Failed to prepare the transaction")
+        # Query the stock service to check if the items are available
+        for item_id, count in items_count.items():
+            response = requests.get(f"{gateway_url}/stock/find/{item_id}")
+            if response.status_code != 200:
+                abort(response.status_code, description=f"Failed to check stock for item {item_id}")
+            stock_data = response.json()
+            if stock_data["stock"] < count:
+                abort(400, description=f"Not enough stock for item {item_id}")
+        # Call the coordinator service to acknowledge the transaction
+        response = requests.post(f"{gateway_url}/acknowledge/{conn_id}")
+        if response.status_code != 200:
+            abort(response.status_code, description="Failed to acknowledge the transaction")
+        # Query the payment service to process the payment
+        response = requests.post(f"{gateway_url}/payment/pay/{order_found['user_id']}/{order_id}/{int(order_found['total_cost'])}")
+        if response.status_code != 200:
+            abort(response.status_code, description="Payment failed")
+        # Call the coordinator service to commit the transaction
+        response = requests.post(f"{gateway_url}/commit_tx/{conn_id}")
+        if response.status_code != 200:
+            abort(response.status_code, description="Failed to commit the transaction")
+        # Update the stock information and order information
+        for item_id, count in items_count.items():
+            response = requests.post(f"{gateway_url}/stock/subtract/{item_id}/{count}")
             if response.status_code != 200:
                 abort(response.status_code, description="Stock update failed")
+        # Mark the order as paid
+        order_found["paid"] = True
+        db.set(order_id, json.dumps(order_found))
+        return {"status": "success"}, 200
+    except:
+        # If any error occurs, cancel the transaction
+        response = requests.post(f"{gateway_url}/cancel_tx/{conn_id}")
+        if response.status_code != 200:
+            abort(response.status_code, description="Failed to cancel the transaction")
 
-    order_found["paid"] = True
-    # order_found["total_cost"] = total_cost
-    db.set(order_id, json.dumps(order_found))
-
-    return {"status": "success"}, 200
-
+        abort(500, description="Transaction aborted")
 
